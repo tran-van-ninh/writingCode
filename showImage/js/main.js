3c494579b2a1ad11e1e28c54bf8b85afb6200c2b
tran-van-ninh <dinhmanhninhtt@gmail.com>
2024-06-14 10:43:08 +0700

    showImage continue

--- showImage/js/main.js
+++ showImage/js/main.js
@@ -1,6 +1,7 @@
 var $ = document.querySelector.bind(document);
 var $$ = document.querySelectorAll.bind(document);
 
+var wrapperImage = $('.wrapper')
 var images = $$('.image img');
 var prev = $('.control__prev');
 var next = $('.control__next');
@@ -13,18 +14,19 @@ var currentIndex = 0;
 
 function showGallery(){
     if(currentIndex == 0){
-        prev.classList.add('hide');
+        prev.classList.add('hidden');
     }else{
-        prev.classList.remove('hide')
+        prev.classList.remove('hidden')
     }
 
     if(currentIndex == lengthImage - 1){
-        next.classList.add('hide');
+        next.classList.add('hidden');
     }else{
-        next.classList.remove('hide')
+        next.classList.remove('hidden')
     }
     galleryImg.src = images[currentIndex].src;
     gallery.classList.add('show__image')
+    wrapperImage.classList.add('hidden');
 }
 images.forEach((item, index) => {
     item.addEventListener('click',function(e) {
@@ -35,12 +37,14 @@ images.forEach((item, index) => {
 });
 
 close.addEventListener('click',()=> {
-    gallery.classList.remove('show__image');                                                              
+    gallery.classList.remove('show__image');
+    wrapperImage.classList.remove('hidden');                                                         
 })
 
 document.addEventListener('keydown',(e) =>{
     if(e.keyCode == 27){
-        gallery.classList.remove('show__image'); 
+        gallery.classList.remove('show__image');
+        wrapperImage.classList.remove('hidden');
     }
 })
 
